52ff5cde0e427b40c4246420a0c65225457535b7
BNNJ <BNNJ1974@gmail.com>
2021-05-14 16:54:26 +0200

    untangling knots

--- chess.py
+++ chess.py
@@ -4,58 +4,40 @@ import operator
 from model import *
 from tinydb import *
 
-def swiss_sort(players, prev, r):
-	players = sorted(players, key=operator.attrgetter('rank'))
-	if r > 0:
-		players = sorted(players, key=operator.attrgetter('score'))
-		if players[1].id in prev[players[0]]:
-			players[0], players[1] = players[1], players[0]
-	players = [p.id for p in players]
-	pivot = len(players) // 2
-	matchups = list(zip(players[:pivot], players[pivot:]))
-	for b, w in matchups:
-		prev[b].append(w)
-		prev[w].append(b)
-	return matchups
+
+def swiss_sort(players, prev, score, r):
+    players = sorted(players, key=operator.attrgetter('rank'))
+    if r > 0:
+        players = sorted(players, key=operator.attrgetter('score'))
+        if players[1].id in prev[players[0].id]:
+            players[0], players[1] = players[1], players[0]
+    players = [p.id for p in players]
+    pivot = len(players) // 2
+    matchups = list(zip(players[:pivot], players[pivot:]))
+    for b, w in matchups:
+        prev[b].append(w)
+        prev[w].append(b)
+    return matchups
+
 
 def main():
-	# clear_table('players.json')
-	# players = [
-	# 	new_player("John Paul", "Jones", "M", "0", 14),
-	# 	new_player("John", "Bonham", "M", "0",4),
-	# 	new_player("Jimmy", "Page", "M", "0", 25),
-	# 	new_player("Robert", "Plant", "M", "0", 8),
-	# 	new_player("Ritchie", "Blackmore", "M", "0", 6),
-	# 	new_player("Ian", "Paice", "M", "0", 2),
-	# 	new_player("Ian", "Gillan", "M", "0", 21),
-	# 	new_player("Roger", "Glover", "M", "0", 19),
-	# ]
-	# [p.save() for p in players]
-	# [print(p.id) for p in players]
-	
+    players = get_players(range(1, 9))
 
-	print(get_player_by_name("Ian", "Paice"))
+    [print(p) for p in players]
+    print("\n##################\n")
 
+    previous_matchups = {p.id: [] for p in players}
+    score = {p.id: 0 for p in players}
+    for round_count in range(5):
+        matchups = swiss_sort(players, previous_matchups, score, round_count)
+    #   print(f"round {round_count + 1}:")
+    #   [print(b, w) for (b, w) in matchups]
+    #   for p in players:
+    #       p.score += randrange(30)
+    # print(previous_matchups)
 
-	# tournament = Tournament(
-	# 	name = "whatev",
-	# 	location = "Djakarta",
-	# 	date = "123",
-	# 	players = players,
-	# 	time_format = TimeFormat.BLITZ,
-	# )
+    # [print(p) for p in get_players()]
 
-	# previous_matchups = {p.id: [] for p in players}
-
-	# for round_count in range(0, tournament.rounds_max):
-	# 	matchups = swiss_sort(players, previous_matchups, round_count)
-	# 	print(f"round {round_count + 1}:")
-	# 	[print(b, w) for (b, w) in matchups]
-	# 	for p in players:
-	# 		p.score += randrange(30)
-		# print(previous_matchups)
-
-	# [print(p) for p in get_players()]
 
 if __name__ == "__main__":
-	main()
+    main()
--- model.py
+++ model.py
@@ -1,147 +1,203 @@
 #!/usr/bin/env python3
 
-from tinydb import TinyDB, Query, where
+from tinydb import TinyDB, where
 from enum import Enum
 
-class Gender(Enum):
-	UNKNOWN = 0
-	MALE = 1
-	FEMALE = 2
 
-class TimeFormat(Enum):
-	BLITZ = 0
-	BULLET = 1
-	FAST = 2
+# class Gender(Enum):
+#     UNKNOWN = 0
+#     MALE = 1
+#     FEMALE = 2
 
-#################################### PLAYER ####################################
+
+# class TimeFormat(Enum):
+#     BLITZ = 0
+#     BULLET = 1
+#     FAST = 2
+
+
+###############################################################################
+# PLAYER
+###############################################################################
 
 class Player:
-	def __init__(self, first_name, last_name, gender, birth_date, rank, _id=0):
-		self.first_name = first_name.title()
-		self.last_name = last_name.title()
-		self.birth_date = birth_date
-		self.rank = rank
-		if gender.lower() in ["m", "male", "man", "boy", "guy", "dude"]:
-			self.gender = Gender.MALE
-		elif gender.lower() in ["f", "female", "woman", "girl"]:
-			self.gender = Gender.FEMALE
-		else:
-			self.gender = Gender.UNKNOWN
-		self.id = _id
+    def __init__(self, first_name, last_name, gender,
+                 birth_date, rank, _id=None):
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.birth_date = birth_date
+        self.rank = rank
+        self.gender = gender.lower()
+        self.id = _id
+        self.score = 0
 
-	def serialize(self):
-		d = self.__dict__
-		d['gender'] = "M" if self.gender == Gender.MALE else "F"
-		return d
+    def serialize(self):
+        return self.__dict__
 
-	def save(self):
-		db = TinyDB('players.json')
-		player = db.get(
-			(where('first_name') == self.first_name)
-			& (where('last_name') == self.last_name)
-		)
-		if player is None:
-			db.insert(self.serialize())
-		else:
-			db.update(self.serialize(), doc_ids=[player.doc_id])
+    def save(self):
+        db = TinyDB('players.json')
+        player = db.get(
+            (where('first_name') == self.first_name)
+            & (where('last_name') == self.last_name)
+        )
+        if player is None:
+            db.insert(self.serialize())
+        else:
+            db.update(self.serialize(), doc_ids=[player.doc_id])
 
-	def update(self, value):
-		TinyDB('players.json').upsert(
-			value,
-			Query().first_name == self.first_name
-		)
+    def change_rank(self, rank):
+        self.rank = rank
 
-	def change_rank(self, rank):
-		self.rank = rank
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name} ({self.id}):"
+                f" rank:{self.rank}")
 
-	def __str__(self):
-		return (f"{self.first_name} {self.last_name} ({self.id}): rank:{self.rank}")
-	def __repr__(self):
-		return (f"{self.first_name} {self.last_name} ({self.id}): rank:{self.rank}")
+    def __repr__(self):
+        return (f"{self.first_name} {self.last_name} ({self.id}):"
+                f" rank:{self.rank}")
+
 
 # Player functions
 
 def new_player(first_name, last_name, gender, birth_date, rank):
-	return Player(first_name, last_name, gender, birth_date, rank)
+    return Player(first_name, last_name, gender, birth_date, rank)
+
 
 def get_player_by_name(first_name, last_name):
-	player = TinyDB('players.json').get(
-		(where('first_name') == first_name.title())
-		& (where('last_name') == last_name.title())
-	)
-	if player is not None:
-		return Player(
-			player['first_name'],
-			player['last_name'],
-			player['gender'],
-			player['birth_date'],
-			player['rank'],
-			_id=player.doc_id
-		)
-	else:
-		return None
+    player = TinyDB('players.json').get(
+        (where('first_name') == first_name.title())
+        & (where('last_name') == last_name.title())
+    )
+    if player is not None:
+        return Player(
+            player['first_name'],
+            player['last_name'],
+            player['gender'],
+            player['birth_date'],
+            player['rank'],
+            player.doc_id
+        )
+    else:
+        return None
+
 
 def get_players_by_name(names):
-	return [get_player_by_name(fn, ln) for (fn, ln) in names]
+    return [get_player_by_name(fn, ln) for (fn, ln) in names]
+
 
 def get_player(_id):
-	player = TinyDB('players.json').get(doc_id=_id)
-	if player is not None:
-		return Player(
-			player['first_name'],
-			player['last_name'],
-			player['gender'],
-			player['birth_date'],
-			player['rank'],
-			_id=player.doc_id
-		)
-	else:
-		return None
+    player = TinyDB('players.json').get(doc_id=_id)
+    if player is not None:
+        return Player(
+            player['first_name'],
+            player['last_name'],
+            player['gender'],
+            player['birth_date'],
+            player['rank'],
+            player.doc_id
+        )
+    else:
+        return None
+
 
 def get_players(ids):
-	return [get_player(i) for i in ids]
+    return [get_player(i) for i in ids]
 
 
-#################################### MATCH #####################################
+def show_players():
+    [print(p) for p in TinyDB('players.json').all()]
 
-class Round:
-	pass
+
+###############################################################################
+# MATCH
+###############################################################################
 
 class Match:
-	pass
+    def __init__(self, black, white, result=None):
+        self.black = black
+        self.white = white
+        self.result = result
 
 
-################################## TOURNAMENT ##################################
+###############################################################################
+# ROUND
+###############################################################################
+
+class Round:
+    def __init__(self, matches=[]):
+        self.matches = matches
+
+
+###############################################################################
+# TOURNAMENT
+###############################################################################
 
 class Tournament:
-	def __init__(self, name, location, date, players, time_format,
-				description="", rounds_max=4, _id=0):
-		self.name = name
-		self.location = location
-		self.date = date
-		self.players = players
-		self.players_count = len(players)
-		self.time_format = time_format
-		self.description = description
-		self.rounds_max = rounds_max
-		self.matches = []
-		self.rounds = []
-		self.id = _id
+    def __init__(self, name, location, date, players, time_format,
+                 description="", rounds_max=4, matches=[],
+                 rounds=[], score={}, _id=None):
+        self.name = name
+        self.location = location
+        self.date = date
+        self.players = players
+        self.players_count = len(players)
+        self.time_format = time_format
+        self.description = description
+        self.rounds_max = rounds_max
+        self.matches = matches
+        self.rounds = rounds
+        self.score = score
+        self.id = _id
 
-	def serialize(self):
-		return self.__dict__
+    def serialize(self):
+        return self.__dict__
 
-	def save(self):
-		db = TinyDB('tournaments.json')
-		tourney = db.get(where('name') == self.name)
-		if tourney is None:
-			db.insert(self.serialize())
-		else:
-			db.update(self.serialize(), doc_ids=[tourney.doc_id])
+    def save(self):
+        db = TinyDB('tournaments.json')
+        tourney = db.get(where('name') == self.name)
+        if tourney is None:
+            db.insert(self.serialize())
+        else:
+            db.update(self.serialize(), doc_ids=[tourney.doc_id])
+
+    def update_scores(self, players):
+        self.score = {p.id: p.score for p in players}
+
+    def get_players(self):
+        players = get_players(self.players)
+        for p in players:
+            p.score = self.score[p.id]
+        return players
+
+    def __str__(self):
+        return f"{self.name} ({self.id}): {self.location} on {self.date}"
+
+    def __repr__(self):
+        return f"{self.name} ({self.id}): {self.location} on {self.date}"
 
 
+def new_tournament(name, location, date, players, time_format):
+    return Tournament(name, location, date, players, time_format)
 
-################################################################################
+
+def load_tournament(name):
+    tourney = TinyDB('tournaments.json').get(where('name') == name)
+    return Tournament(
+        tourney['name'],
+        tourney['location'],
+        tourney['date'],
+        tourney['players'],
+        tourney['time_format'],
+        tourney['description'],
+        tourney['rounds_max'],
+        tourney['matches'],
+        tourney['rounds'],
+        tourney['score'],
+        _id=tourney.doc_id
+    )
+
+
+###############################################################################
 
 def clear_table(table):
-	TinyDB(table).truncate()
+    TinyDB(table).truncate()
