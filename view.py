3866fb0ddc6869e5b48815e2414d52071f99fcaf
BNNJ <BNNJ1974@gmail.com>
2021-05-25 19:49:06 +0200

    improved date input, added long input and input validation

--- view.py
+++ view.py
@@ -12,6 +12,7 @@ KEY_UP = curses.KEY_UP
 KEY_DOWN = curses.KEY_DOWN
 KEY_LEFT = curses.KEY_LEFT
 KEY_RIGHT = curses.KEY_RIGHT
+KEY_BACK = [curses.KEY_BACKSPACE, ord('\b'), ord('\x7f')]
 
 DIRECTIONS = {
     KEY_UP: -1,
@@ -45,6 +46,9 @@ class Win:
 
     def addstr(self, y, x, s):
         self.win.addstr(y, x, s)
+
+    def move(self, y, x):
+        self.win.move(y, x)
 
     def clear(self):
         self.win.clear()
@@ -119,7 +123,111 @@ class InputWin(Win):
         curses.noecho()
         return str(result, 'utf-8')
 
-    def choice_field(self, y, title, *options):
+    def get_int(self, y):
+        curses.curs_set(1)
+        buff = ""
+        y += 1
+        x = 2
+        self.move(y, x)
+        while True:
+            c = self.getch()
+            if c in KEY_ENTER:
+                break
+            elif c in KEY_BACK:
+                buff = buff[:-1]
+                x -= 1
+                self.addstr(y, x, " ")
+                self.move(y, x)
+            elif x == 30:
+                pass
+            elif ord('0') <= c <= ord('9'):
+                buff += chr(c)
+                self.win.addstr(chr(c))
+                x += 1
+        curses.curs_set(0)
+        return f"{buff[:2]}/{buff[2:4]}/{buff[4:8]}"
+
+
+    def date_field(self, y, title):
+        curses.textpad.rectangle(self.win, y, 0, y + 2, 13)
+        self.addstr(y, 1, f" {title} ")
+        self.addstr(y + 1, 2, f"  /  /    ")
+
+    def get_date(self, y):
+        curses.curs_set(1)
+        buff = ""
+        y += 1
+        x = 2
+        self.move(y, x)
+        while True:
+            c = self.getch()
+            if c in KEY_ENTER:
+                if date_is_valid(buff):
+                    break
+                else:
+                    buff = ""
+                    Popup("info", "Invalid date !").draw()
+                    self.draw()
+                    self.refresh()
+                    x = 2
+                    self.move(y, x)
+            elif c in KEY_BACK:
+                buff = buff[:-1]
+                if 2 < x < 13:
+                    x -= 1
+                if x in [4, 7]:
+                    x -= 1
+                self.addstr(y, x, " ")
+                self.move(y, x)
+            elif x == 12:
+                pass
+            elif ord('0') <= c <= ord('9'):
+                buff += chr(c)
+                self.win.addstr(chr(c))
+                x += 1
+                if x in [4, 7]:
+                    x += 1
+                self.move(y, x)
+        curses.curs_set(0)
+        return f"{buff[:2]}/{buff[2:4]}/{buff[4:8]}"
+
+    def long_field(self, y, title):
+        curses.textpad.rectangle(self.win, y, 0, y + 7, 32)
+        self.addstr(y, 1, f" {title} ")
+
+    def get_long(self, y):
+        curses.curs_set(1)
+        txt = ""
+        start_y = y + 1
+        y, x = 0, 2
+        self.move(start_y, 2)
+        while True:
+            c = self.getch()
+            if c in KEY_ENTER:
+                break
+            elif c in KEY_BACK:
+                txt = txt[:-1]
+                if x > 2:
+                    x -= 1
+                elif y > 0:
+                    x = 30
+                    y -= 1
+                self.addstr(start_y+y, x, " ")
+            elif x >= 30 and y >= 5:
+                pass
+            elif 32 <= c <= 126:
+                txt += chr(c)
+                self.win.addstr(chr(c))
+                if x < 30:
+                    x += 1
+                else:
+                    x = 2
+                    y += 1
+            self.move(start_y+y, x)
+        curses.curs_set(0)
+        return txt
+
+    def choice_menu(self, y, title, *options):
         h = len(options) + 1
         curses.textpad.rectangle(self.win, y, 0, y + h, 32)
         self.addstr(y, 1, f" {title} ")
@@ -144,11 +252,17 @@ class InputWin(Win):
     def draw(self):
         y = 0
         for f in self.args:
-            if f['type'] in ["string", "date", "int"]:
+            if f['type'] in ["string", "int"]:
                 self.input_field(y, f['title'])
                 y += 4
+            elif f['type'] == "date":
+                self.date_field(y, f['title'])
+                y += 4
+            elif f['type'] == "long":
+                self.long_field(y, f['title'])
+                y += 8
             elif f['type'] == "menu":
-                self.choice_field(y, f['title'], *f['options'])
+                self.choice_menu(y, f['title'], *f['options'])
                 y += len(f['options']) + 3
         self.refresh()
 
@@ -156,9 +270,18 @@ class InputWin(Win):
         y = 0
         results = {}
         for f in self.args:
-            if f['type'] in ["string", "date", "int"]:
+            if f['type'] == "string":
                 results[f['name']] = self.get_string(y)
                 y += 4
+            elif f['type'] == "int":
+                results[f['name']] = self.get_int(y)
+                y += 4
+            elif f['type'] == "date":
+                results[f['name']] = self.get_date(y)
+                y += 4
+            elif f['type'] == "long":
+                results[f['name']] = self.get_long(y)
+                y += 8
             elif f['type'] == "menu":
                 results[f['name']] = self.get_choice(y, *f['options'])
                 y += len(f['options']) + 3
@@ -170,15 +293,15 @@ class InputWin(Win):
         for entry in data:
             self.addstr(y, 0, f"{entry}: {data[entry]}")
             y += 1
-        y += 2
-        self.choice_field(y, "save ?", "Yes", "No")
+        y += 4
+        self.choice_menu(y, "save ?", "Yes", "No")
         self.refresh()
         return (self.get_choice(y, "Yes", "No") == "Yes")
 
 
 class Popup(Win):
 
-    def __init__(self, h, w, y, x, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         min_w = 29
         max_w = 58 #maximum line length
         self.message = [args[1][i:max_w+i] for i in range(0, len(args[1]), max_w)]
@@ -186,7 +309,7 @@ class Popup(Win):
         w = max(min_w + 4, len(self.message[0]) + 4)
         y = curses.LINES // 2 - h // 2
         x = curses.COLS // 2 - w // 2
-        super().__init__(h, w, y, x, *args, **kwargs)
+        super().__init__(h, w, y, x, args[0])
 
     def draw(self):
         self.win.box()
@@ -194,7 +317,6 @@ class Popup(Win):
         for y, line in enumerate(self.message):
             x = self.w//2 - len(line)//2
             self.addstr(y + 1, x, line)
-        # self.addstr(1, 1, self.message[0])
         txt = " press any key to continue "
         self.addstr(self.h - 1, self.w//2 - len(txt)//2, txt)
         self.refresh()
@@ -204,6 +326,9 @@ class Popup(Win):
 
 
 ######################################################
+
+def date_is_valid(d):
+    return (len(d) == 8) and (0 < int(d[:2]) <= 31) and (0 < int(d[2:4]) <= 12)
 
 def init(stdscr):
     curses.curs_set(0)
